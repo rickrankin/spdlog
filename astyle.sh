eab7ddc1f777af272d4facbd925511806923cf9b
Rick Rankin <rick.rankin@saifeinc.com>
2017-07-18 14:03:19 -0700

    Make sure to find all the files and ignore any existing .astylerc

--- astyle.sh
+++ astyle.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-find . -name "*\.h" -o -name "*\.cpp"|xargs dos2unix
-find . -name "*\.h" -o -name "*\.cpp"|xargs astyle -n -c -A1 
 
-
+astyleOptions=(
+  "--options=none"                  # Ignore existing .astylerc
+  "--suffix=none"                   # Don't backup the original file
+  "--style=break"                   # Brace style
+  "--convert-tabs"                  # Convert tabs to spaces
+)
+find . \( -name "*.h" -o -name "*.cpp" \) -exec dos2unix {} +
+find . \( -name "*.h" -o -name "*.cpp" \) -exec astyle "${astyleOptions[@]}" {} +
